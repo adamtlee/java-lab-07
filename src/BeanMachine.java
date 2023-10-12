fc1c55eea330e6d7373b028ef73acf0896ba3fe8
adamtlee <adam.lee346@gmail.com>
2023-10-11 20:01:35 -0600

    Initial Commit and Project Setup

---
+++ src/BeanMachine.java
@@ -0,0 +1,22 @@
+public class BeanMachine {
+    private int numberOfLevels;
+    public BeanMachine(int numberOfLevels)
+    {
+        this.numberOfLevels = numberOfLevels;
+    }
+    public void simulateGame(int number)
+    {
+
+    }
+    public int getBinResult(String s)
+    {
+        // Temp
+        return 0;
+    }
+
+    public String getBinDescription()
+    {
+        // Temp
+        return "";
+    }
+}
---
+++ src/Path.java
@@ -0,0 +1,27 @@
+public class Path {
+    private char charOne;
+    private char charTwo;
+    public Path(){
+        this.charOne = 'L';
+        this.charTwo = 'R';
+    }
+
+    public void moveRight(){
+
+    }
+
+    public void moveLeft() {
+
+    }
+
+    public void fallLevels(int number)
+    {
+
+    }
+
+    public String getPath(){
+        // Temp
+        return "";
+    }
+
+}
